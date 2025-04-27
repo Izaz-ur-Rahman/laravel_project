759c80e16d6c3248d91ed4ce9bde2e021e1cf77c
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-04-27 12:24:03 +0500

    create new router with name is demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <body>
-
+<h1>welcome to laravel course </h1>
     </body>
- 
+
 </html>
