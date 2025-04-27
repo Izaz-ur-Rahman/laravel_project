49a04bc3e488eef8df97249ab1a40a507784281c
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-04-27 14:41:38 +0500

    create new blade view with name home

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/demo',function(){
+//     return view('demo');
+// });
+
+// // route with data ...
+// Route::get('demo/{name}/{id?}',function($name,$id= null){
+//     // echo $name;
+//    // echo $id;
+//     $data = compact('name','id');
+//     return view('demo')->with($data);
+// });
+
+Route::get('/{name?}',function($name= null){
+    $data = compact('name');
+return  view('home')->with($data);
 });
 
-Route::get('/demo',function(){
-    return view('demo');
-});
-
-// route with data ...
-Route::get('demo/{name}/{id}',function($name,$id= null){
-    // echo $name;
-   // echo $id;
-    $data = compact('name','id');
-    return view('demo')->with($data);
-});
