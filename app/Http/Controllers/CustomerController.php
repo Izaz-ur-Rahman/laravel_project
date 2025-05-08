c35ca044ce009b1f48a824202d93ba25634772a1
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-05-08 15:53:33 +0500

    insertion in table testing

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,12 +11,12 @@ class CustomerController extends Controller
    }
    public function store(Request $request){
         echo "<pre>";
-        dd($request->all()); // This stops execution and dumps input data
-        $customer = new Customers;
-        $customer->name = $request['name'];
-        $customer->email  = $request['email'];
-        $customer->password = $request['pass'];
-        $customer->save();
+        print_r($request->all()); // This stops execution and dumps input data
+      //   $customer = new Customers;
+      //   $customer->name = $request['name'];
+      //   $customer->email  = $request['email'];
+      //   $customer->password = $request['pass'];
+      //   $customer->save();
 
    }
 }
