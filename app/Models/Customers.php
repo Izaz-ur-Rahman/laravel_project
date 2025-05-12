27b9efd5512a86e243e18078c9f3e2e1835369d7
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-05-12 12:13:08 +0500

    perform soft delete

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Customers extends Model
 {
+    use SoftDeletes;
     protected $table = "customers";
+
     protected $primaryKey = "id";
     protected $fillable = ['name', 'email', 'password'];
 
