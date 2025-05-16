bb0843e8f00604e931c699b048a61dd07fb3b502
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-05-16 11:02:34 +0500

    now use faker for inserting fake data for testing

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Customers;
+use Faker\Factory as Faker;
 class CustomerSeeder extends Seeder
 {
     /**
@@ -12,10 +13,11 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
         $customer = new Customers;
-        $customer->name = "Izaz developer";
-        $customer->email = "Izaz@gmail.com";
-        $customer->password = "123456";
+        $customer->name = $faker->name;
+        $customer->email = $faker->email;
+        $customer->password = $faker->password;
         $customer->save();
 
     }
