fa2b65c380dc122ba05d36cc6d85c192542da6d7
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-05-16 10:53:27 +0500

    write a code in run  function for insertioni

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Customers;
 class CustomerSeeder extends Seeder
 {
     /**
@@ -12,6 +12,11 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $customer = new Customers;
+        $customer->name = "Izaz developer";
+        $customer->email = "Izaz@gmail.com";
+        $customer->password = "123456";
+        $customer->save();
+
     }
 }
